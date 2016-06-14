365ec18d949f4897e05bedd85b32a8dc9177cd08
Romane Vix <vix.romane@gmail.com>
2016-06-15 00:53:58 +0200

    sidebar projet

--- wp-content/themes/theme-plaitil/sidebar.php
+++ wp-content/themes/theme-plaitil/sidebar.php
@@ -7,17 +7,24 @@
  */
 
 ?>
-<aside class="sidebar-projet">
+<aside class="sidebar-projet medium-3 columns small-12 columns">
 <!-- 	<?php do_action( 'foundationpress_before_sidebar' ); ?>
 	<?php dynamic_sidebar( 'sidebar-widgets' ); ?>
 	<?php do_action( 'foundationpress_after_sidebar' ); ?> -->
 
-	<article class="infos-projet">
+	<article class="infos-projet medium-3 small-12 columns">
 
 		<h3>Caractéristiques</h3>
-		<?php echo $value = get_field("caracteristiques");?>
+		<li><?php echo $value = get_field("caracteristiques");?></li>
+		<h3>Date</h3>
+		<time><?php echo $value = get_field("time");?></time>
+
 		
 	</article>
+
+	<div class="titre_decoratif">
+			<h2><?php echo $value = get_field("titre_decoratif");?></h2>
+	</div>
 
 
 </aside>
